1a5a4ac76cc4b3e29ae600f3b512e9782013c8ce
Narbert Hamill <nhamill@il-d6.21-school.ru>
2020-03-02 16:06:14 +0300

    fix fork son with parents num_code and wait parameters

--- srcs/fork.c
+++ srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: nhamill <nhamill@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 14:46:23 by nhamill           #+#    #+#             */
-/*   Updated: 2020/03/02 14:55:48 by nhamill          ###   ########.fr       */
+/*   Updated: 2020/03/02 16:03:26 by nhamill          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ void	cfork(t_crwr *crwr, t_cursor *temp)
 	num = get_direct((unsigned char *)crwr->arena->field, temp->pc + 1, 0);
 	ft_cursor_add(&(crwr->cursor), ft_cursor_fork(temp, looped(temp->pc, num % IDX_MOD), crwr->cursor->id + 1));
 	crwr->cursor->nc = *((unsigned char *)crwr->arena->field + crwr->cursor->pc) - 1;
-	crwr->cursor->wait = (temp->nc < 0x10 ? g_op_tab[temp->nc].wait - 1: 0);
+	crwr->cursor->wait = (crwr->cursor->nc < 0x10 ? g_op_tab[crwr->cursor->nc].wait - 1 : 0);
+//	printf("%u %u %u\n", crwr->cursor->nc, crwr->cursor->wait, crwr->cursor->id & 0x7ffffff);
 }
 
 void	lfork(t_crwr *crwr, t_cursor *temp)
@@ -29,5 +30,6 @@ void	lfork(t_crwr *crwr, t_cursor *temp)
 	num = get_direct((unsigned char *)crwr->arena->field, temp->pc + 1, 0);
 	ft_cursor_add(&(crwr->cursor), ft_cursor_fork(temp, looped(temp->pc, num), crwr->cursor->id));
 	crwr->cursor->nc = *((unsigned char *)crwr->arena->field + crwr->cursor->pc) - 1;
-	crwr->cursor->wait = (temp->nc < 0x10 ? g_op_tab[temp->nc].wait - 1: 0);
+	crwr->cursor->wait = (crwr->cursor->nc < 0x10 ? g_op_tab[crwr->cursor->nc].wait - 1: 0);
+//	printf("%u %u %u\n", crwr->cursor->nc, crwr->cursor->wait, crwr->cursor->id & 0x7ffffff);
 }
